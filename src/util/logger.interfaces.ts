7312c3b4bb3e66571fdde16b98d2ed41461b9366
Drischdaan <42834596+Drischdaan@users.noreply.github.com>
2021-07-08 17:34:24 +0200

    Removed everything

--- src/util/logger.interfaces.ts
+++
@@ -1,5 +0,0 @@
-export interface ILogger {
-  info(message: string, ...args: string[]): void;
-  error(message: string, ...args: string[]): void;
-  warn(message: string, ...args: string[]): void;
-}
